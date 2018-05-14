0c4d6fd80c7f6e697d79dc3d6f6479668546f01f
Marc <marc.j.bouwman@gmail.com>
2018-05-14 18:06:58 +0200

    Check if the request bodies are correct or throw an ApiError

--- routes/studentenhuis.js
+++ routes/studentenhuis.js
@@ -1,36 +1,112 @@
 const express = require("express");
 const router = express.Router();
+const ApiError = require("../model/ApiError.js");
 
 // Studentenhuis
 
-router.route("/").get((request, response) => {});
+function isStudentenHuis(object){
+    return 
+        object && typeof object == "object" && 
+        object.naam && typeof object.naam == "string" && 
+        object.adres && typeof object.adres == "string";
+}
 
-router.route("/").post((request, response) => {});
+router.route("/").get((request, response) => {
+    
+});
 
-router.route("/:huisId?").get((request, response) => {});
+router.route("/").post((request, response) => {
+    try {
+        const studentenhuis = request.body;
 
-router.route("/:huisId?").put((request, response) => {});
+        if(!isStudentenHuis(studentenhuis))
+            throw new ApiError("Een of meer properties in de request body ontbreken of zijn foutief", 412);
 
-router.route("/:huisId?").delete((request, response) => {});
+    } catch (error){
+        response.status(error.code).json(error);
+    }
+});
+
+router.route("/:huisId?").get((request, response) => {
+
+});
+
+router.route("/:huisId?").put((request, response) => {
+    try {
+        const studentenhuis = request.body;
+
+        if(!isStudentenHuis(studentenhuis))
+            throw new ApiError("Een of meer properties in de request body ontbreken of zijn foutief", 412);
+
+    } catch (error){
+        response.status(error.code).json(error);
+    }
+});
+
+router.route("/:huisId?").delete((request, response) => {
+
+});
 
 // Maaltijd
 
-router.route("/:huisId?/maaltijd").get((request, response) => {});
+function isMaaltijd(object){
+    return 
+        object && typeof object == "object" && 
+        object.naam && typeof object.naam == "string" && 
+        object.beschrijving && typeof object.beschrijving == "string" &&
+        object.ingredienten && typeof object.ingredienten == "string" &&
+        object.allergie && typeof object.allergie == "string" &&
+        object.prijs && typeof object.prijs == "number";
+}
 
-router.route("/:huisId?/maaltijd").post((request, response) => {});
+router.route("/:huisId?/maaltijd").get((request, response) => {
 
-router.route("/:huisId?/maaltijd/:maaltijdId?").get((request, response) => {});
+});
 
-router.route("/:huisId?/maaltijd/:maaltijdId?").put((request, response) => {});
+router.route("/:huisId?/maaltijd").post((request, response) => {
+    try {
+        const maaltijd = request.body;
+        
+        if(!isMaaltijd(maaltijd))
+            throw new ApiError("Een of meer properties in de request body ontbreken of zijn foutief", 412);
 
-router.route("/:huisId?/maaltijd/:maaltijdId?").delete((request, response) => {});
+    } catch (error){
+        response.status(error.code).json(error);
+    }
+});
+
+router.route("/:huisId?/maaltijd/:maaltijdId?").get((request, response) => {
+
+});
+
+router.route("/:huisId?/maaltijd/:maaltijdId?").put((request, response) => {
+    try {
+        const maaltijd = request.body;
+        
+        if(!isMaaltijd(maaltijd))
+            throw new ApiError("Een of meer properties in de request body ontbreken of zijn foutief", 412);
+
+    } catch (error){
+        response.status(error.code).json(error);
+    }
+});
+
+router.route("/:huisId?/maaltijd/:maaltijdId?").delete((request, response) => {
+
+});
 
 // Deelnemers
 
-router.route("/:huisId?/maaltijd/:maaltijdId?/deelnemers").get((request, response) => {});
+router.route("/:huisId?/maaltijd/:maaltijdId?/deelnemers").get((request, response) => {
 
-router.route("/:huisId?/maaltijd/:maaltijdId?/deelnemers").post((request, response) => {});
+});
 
-router.route("/:huisId?/maaltijd/:maaltijdId?/deelnemers").delete((request, response) => {});
+router.route("/:huisId?/maaltijd/:maaltijdId?/deelnemers").post((request, response) => {
+
+});
+
+router.route("/:huisId?/maaltijd/:maaltijdId?/deelnemers").delete((request, response) => {
+    
+});
 
 module.exports = router;

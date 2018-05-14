5e499007f51bf4c18839c729d48e56aba2f9a166
Marc <marc.j.bouwman@gmail.com>
2018-05-14 17:40:25 +0200

    Add endpoints

--- routes/studentenhuis.js
+++ routes/studentenhuis.js
@@ -1,4 +1,36 @@
 const express = require("express");
-const router = express.Router({});
+const router = express.Router();
+
+// Studentenhuis
+
+router.route("/").get((request, response) => {});
+
+router.route("/").post((request, response) => {});
+
+router.route("/:huisId?").get((request, response) => {});
+
+router.route("/:huisId?").put((request, response) => {});
+
+router.route("/:huisId?").delete((request, response) => {});
+
+// Maaltijd
+
+router.route("/:huisId?/maaltijd").get((request, response) => {});
+
+router.route("/:huisId?/maaltijd").post((request, response) => {});
+
+router.route("/:huisId?/maaltijd/:maaltijdId?").get((request, response) => {});
+
+router.route("/:huisId?/maaltijd/:maaltijdId?").put((request, response) => {});
+
+router.route("/:huisId?/maaltijd/:maaltijdId?").delete((request, response) => {});
+
+// Deelnemers
+
+router.route("/:huisId?/maaltijd/:maaltijdId?/deelnemers").get((request, response) => {});
+
+router.route("/:huisId?/maaltijd/:maaltijdId?/deelnemers").post((request, response) => {});
+
+router.route("/:huisId?/maaltijd/:maaltijdId?/deelnemers").delete((request, response) => {});
 
 module.exports = router;

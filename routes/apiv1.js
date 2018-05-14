4c6591874111be29f92822131a1f22ab183944c9
Marc <marc.j.bouwman@gmail.com>
2018-05-14 15:29:42 +0200

    Add routes for authentication and studentenhuis

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -1,4 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
+app.use("/", require("./authentication.js"))
+app.use("/studentenhuis", require("./studentenhuis.js"))
+
 module.exports = router

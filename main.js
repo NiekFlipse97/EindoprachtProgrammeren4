8fbfb395f9d3881a31cc75dd1b996db316c49377
Marc <marc.j.bouwman@gmail.com>
2018-05-14 14:41:56 +0200

    Add basic server functionality

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.all("*", function(req, res, next){
+    // Log all requests
+    console.log(`${req.method} - ${req.url}`)
+    next()
+})
+
+app.use("/api", require("./routes/apiv1.js"))
+
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log(`Connected to port ${port}`)
+})
+
+module.exports = app

01694fdf0d3bcd30534d5d46e2275c85e9c5c9c4
Marc <marc.j.bouwman@gmail.com>
2018-05-14 19:49:50 +0200

    Add new Errors to ApiErrors

--- model/ApiErrors.js
+++ model/ApiErrors.js
@@ -1,3 +1,5 @@
+const ApiError = require("./ApiError.js");
+
 module.exports = class ApiErrors {
     static get notAuthorised(){
         return new ApiError("Niet geautoriseerd (geen valid token)", 401);
@@ -5,5 +7,13 @@ module.exports = class ApiErrors {
 
     static get wrongRequestBodyProperties(){
         return new ApiError("Een of meer properties in de request body ontbreken of zijn foutief", 412);
+    }
+
+    static notFound(objectName){
+        return new ApiError(`Niet gevonden (${objectName} bestaat niet)`, 404);
+    }
+
+    static conflict(message){
+        return new ApiError(`Conflict (${message})`, 409);
     }
 }

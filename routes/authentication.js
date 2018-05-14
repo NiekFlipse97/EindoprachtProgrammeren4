1c935f3aa92675659357b8f5f6c2d34fa6cbae00
Niek Flipse <npaflips@avans.nl>
2018-05-14 17:14:53 +0200

    Added code to validate the token

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,9 +1,27 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({});
+const auth = require('../auth/authentication');
 
 router.all(new RegExp("^(?!\/login$|\/register$).*"), (request, response, next) => {
-    // TODO: Check if token is valid
-    next()
+    console.log("Validate Token");
+
+    // Get the token from the request header.
+    let token = request.header('X-Access-Token');
+
+    auth.decodeToken(token, (error, payload) => {
+        if (error) {
+            // Print the error message to the console.
+            console.log('Error handler: ' + error.message);
+
+            // Set the response's status to error.status or 401(Unauthorised).
+            // Return json to the response with an error message.
+            response.status((error.status || 401)).json({
+                error: new Error("Not authorised").message
+            })
+        } else {
+            next();
+        }
+    });
 });
 
 router.route("/register").post((request, response) => {

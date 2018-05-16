a8cd136c961e0b824c9811196b8ddc013c3fe0a9
Niek Flipse <npaflips@avans.nl>
2018-05-16 21:32:35 +0200

    Passwords are now encrypted. login with plain passwords works too

--- routes/authentication.js
+++ routes/authentication.js
@@ -4,6 +4,7 @@ const auth = require('../auth/authentication');
 const db = require('../db/mysql-connector');
 const ApiErrors = require("../model/ApiErrors.js");
 const Isemail = require('isemail');
+const bcrypt = require('bcrypt');
 
 router.all(new RegExp("^(?!\/login$|\/register$).*"), (request, response, next) => {
     console.log("Validate Token");
@@ -25,35 +26,58 @@ router.all(new RegExp("^(?!\/login$|\/register$).*"), (request, response, next) 
     });
 });
 
-function login(email, password, callback){
+function login(email, password, hash, callback) {
     // Check in database for matching username and password.
-    db.query(`SELECT * FROM user WHERE Email = "${email}" AND Password = "${password}"`, (error, rows, fields) => {
-        if(error) console.log(`Error on login query: ${error.message}, ${error.sqlMessage}`);
-        if(error) callback(error, null);
-        else if(rows.length == 0) callback(ApiErrors.wrongRequestBodyProperties, null);
-        else callback(null, {
-            token: auth.encodeToken(email),
-            email: email
+    if (password !== hash) {
+        bcrypt.compare(password, hash, (error, res) => {
+            if (res) {
+                genereteTokenAfterLogin(email, hash, callback);
+                // db.query(`SELECT * FROM user WHERE Email = "${email}" AND Password = "${hash}"`, (error, rows, fields) => {
+                //     if (error) console.log(`Error on login query: ${error.message}, ${error.sqlMessage}`);
+                //     if (error) callback(error, null);
+                //     else if (rows.length == 0) callback(ApiErrors.wrongRequestBodyProperties, null);
+                //     else callback(null, {
+                //             token: auth.encodeToken(email),
+                //             email: email
+                //         });
+                // });
+            } else {
+                console.log(error);
+            }
         });
+    } else {
+        genereteTokenAfterLogin(email, password, callback);
+    }
+}
+
+function genereteTokenAfterLogin(email, password, callback) {
+    db.query(`SELECT * FROM user WHERE Email = "${email}" AND Password = "${password}"`, (error, rows, fields) => {
+        if (error) console.log(`Error on login query: ${error.message}, ${error.sqlMessage}`);
+        if (error) callback(error, null);
+        else if (rows.length == 0) callback(ApiErrors.wrongRequestBodyProperties, null);
+        else callback(null, {
+                token: auth.encodeToken(email),
+                email: email
+            });
     });
 }
 
 class CheckObjects {
     // Returns true if the given object is a valid login
-    static isValidLogin(object){
-        const tmp = 
-            object && typeof object == "object" && 
-            object.email && typeof object.email == "string" && 
+    static isValidLogin(object) {
+        const tmp =
+            object && typeof object == "object" &&
+            object.email && typeof object.email == "string" &&
             object.password && typeof object.password == "string";
         console.log(`Is login valid: ${tmp == undefined ? false : tmp}`);
         return tmp == undefined ? false : tmp;
     }
 
     // Returns true if the given object is a valid register
-    static isValidRegistration(object){
-        const tmp = 
-            object && typeof object == "object" && 
-            object.firstname && typeof object.firstname == "string" && object.firstname.length >= 2 && 
+    static isValidRegistration(object) {
+        const tmp =
+            object && typeof object == "object" &&
+            object.firstname && typeof object.firstname == "string" && object.firstname.length >= 2 &&
             object.lastname && typeof object.lastname == "string" && object.lastname.length >= 2 &&
             object.email && typeof object.email == "string" && Isemail.validate(object.email) &&
             object.password && typeof object.password == "string";
@@ -64,7 +88,7 @@ class CheckObjects {
 
 router.route("/register").post((request, response) => {
     const registration = request.body;
-    if(!CheckObjects.isValidRegistration(registration)){
+    if (!CheckObjects.isValidRegistration(registration)) {
         const error = ApiErrors.wrongRequestBodyProperties;
         response.status(error.code).json(error);
         return;
@@ -75,41 +99,48 @@ router.route("/register").post((request, response) => {
     const email = registration.email;
     const password = registration.password;
 
-    db.query(`SELECT * FROM user WHERE Voornaam = "${firstName}" AND Achternaam = "${lastName}" AND Email = "${email}" AND Password = "${password}"`, (error, rows, fields) => {
-        if(rows.length > 0){
-            // User already exists.
-            const error = ApiErrors.notAuthorised
-            response.status(error.code).json(error);
-        } else {
-            // Create the query that will be executed.
-            const query = {
-                sql: 'INSERT INTO user (Voornaam, Achternaam, Email, Password) VALUES(?, ?, ?, ?)',
-                values: [firstName, lastName, email, password],
-                timeout: 2000
-            };
-
-            // Execute the insert query
-            db.query(query, (error, rows, fields) => {
-                // If there is no error
-                if (!error) {
-                    login(email, password, (error, result) => {
-                        if(error) response.status(error.code || 500).json(error);
-                        else response.status(200).json(result);
-                    });
-                } else { 
-                    // If there is an error.
-                    // Set the status to 500 (error.code), and return the error message.
-                    const error = ApiErrors.other(error.message);
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(password, salt, function (err, hash) {
+            console.log("Hash ===== " + hash);
+            // Store hash in your password DB.
+            db.query(`SELECT * FROM user WHERE Voornaam = "${firstName}" AND Achternaam = "${lastName}" AND Email = "${email}" AND Password = "${hash}"`, (error, rows, fields) => {
+                if (rows.length > 0) {
+                    // User already exists.
+                    const error = ApiErrors.notAuthorised;
                     response.status(error.code).json(error);
+                } else {
+                    // Create the query that will be executed.
+                    const query = {
+                        sql: 'INSERT INTO user (Voornaam, Achternaam, Email, Password) VALUES(?, ?, ?, ?)',
+                        values: [firstName, lastName, email, hash],
+                        timeout: 2000
+                    };
+
+                    // Execute the insert query
+                    db.query(query, (error, rows, fields) => {
+                        // If there is no error
+                        if (!error) {
+                            login(email, password, hash, (error, result) => {
+                                if (error) response.status(error.code || 500).json(error);
+                                else response.status(200).json(result);
+                            });
+                        } else {
+                            // If there is an error.
+                            // Set the status to 500 (error.code), and return the error message.
+                            const error = ApiErrors.other(error.message);
+                            response.status(error.code).json(error);
+                        }
+                    });
                 }
             });
-        }
+        });
     });
+
 });
 
 router.route("/login").post((request, response) => {
     const loginObject = request.body;
-    if(!CheckObjects.isValidLogin(loginObject)){
+    if (!CheckObjects.isValidLogin(loginObject)) {
         const error = ApiErrors.wrongRequestBodyProperties;
         response.status(error.code).json(error);
         return;
@@ -118,10 +149,16 @@ router.route("/login").post((request, response) => {
     const email = loginObject.email;
     const password = loginObject.password;
 
-    // Check in database for matching username and password.
-    login(email, password, (error, result) => {
-        if(error) response.status(error.code || 500).json(error);
-        else response.status(200).json(result);
+    db.query("SELECT Password FROM user WHERE Email = ?", [email], (error, rows, fields) => {
+        if (error) {
+            response.status(error.code).json(error.message);
+        }
+
+        // Check in database for matching username and password.
+        login(email, password, rows[0].Password, (error, result) => {
+            if (error) response.status(error.code || 500).json(error);
+            else response.status(200).json(result);
+        });
     });
 });
 
